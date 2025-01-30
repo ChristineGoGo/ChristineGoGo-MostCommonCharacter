0f50cd25e68a899ec452706a2336a71033ff9089
Christine Gogo <brandywang22@gmail.com>
2025-01-30 14:46:38 +0000

    Find the most common character in a string

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class MostCommonCharacter {
     /**
      * Find the most common character in str.
@@ -8,6 +8,37 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        // return ' ';
+        Map<Character, Integer> freq = new HashMap<>(getCharacterFrequency(str));
+        int currentCount = 0;
+        char currentMaxChar = ' ';
+
+        for (Map.Entry<Character, Integer> e: freq.entrySet()) {
+            if (e.getValue() > currentCount) {
+                currentCount = e.getValue();
+                currentMaxChar = (char) e.getKey();
+            }
+        }
+        return currentMaxChar;
+
+     }
+
+    public Map<Character, Integer> getCharacterFrequency(String str) {
+        Map<Character, Integer> frequencyCount = new HashMap<>();
+        char currentChar;
+        int currentCount = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            currentChar = str.charAt(i);
+            if (frequencyCount.containsKey(currentChar)) {
+                currentCount = frequencyCount.get(currentChar);
+                currentCount += 1;
+                frequencyCount.put(currentChar, currentCount);
+            } else {
+                frequencyCount.put(currentChar, 1);
+            }
+            currentChar = ' ';
+        }
+        return frequencyCount;
     }
 }
